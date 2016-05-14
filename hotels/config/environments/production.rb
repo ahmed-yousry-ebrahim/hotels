ceb4505a645e01451360eca1d174ccff9860aa95
Ahmed Yousry <ahmed.yousry@intellisc.com>
2016-05-14 13:53:38 +0200

    change production api base url

--- hotels/config/environments/production.rb
+++ hotels/config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.api_server_url = "http://localhost:3001"
+  config.api_server_url = "https://hotels-api.herokuapp.com"
 end
